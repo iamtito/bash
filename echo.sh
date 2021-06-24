b9436fc01b7c5bb4d7e643c7fe9ab2a3a83f66ef
Kabir Bolatito <tbola45@gmail.com>
2021-06-24 17:42:20 -0400

    Understanding echo

---
+++ echo.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# This is a comment
+# This section prints hello world to the terminal
+echo "************"
+echo "Hello world."
+echo "************"
+echo
+# This section starts the shell script
+echo "The begining of everything."
+echo
+# This is a vim comment
+echo "This section was written by a vim editor."
+
+# This is a nano comment
+echo "This section was written by a nano editor."
